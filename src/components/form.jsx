00f45587a082c510d8f73bc2e400cbf916da1a83
Cameron Slupeiks <cslupeiks@gmail.com>
2020-02-09 15:40:20 -0500

    Test Build

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { FormGroup } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
@@ -30,61 +27,20 @@ export default () => {
 		<div className={classes.container}>
 			<Grid item xs={12}>
 				<Grid item xs={12}>
-					<FormGroup className={classes.formGroup} noValidate autoComplete='on' name='contact' method='post'>
+					<form className='ui form' name='contact' method='post'>
 						<input type='hidden' name='form-name' value='contact' />
-						<TextField
-							className='textfield'
-							type='text'
-							name='first'
-							id='outlined-basic'
-							label='First name'
-							variant='outlined'
-							color='secondary'
-							required
-						/>
-						<TextField
-							className='textfield'
-							type='text'
-							name='last'
-							id='outlined-basic'
-							label='Last Name'
-							variant='outlined'
-							color='secondary'
-						/>
-						<TextField
-							className='textfield'
-							type='email'
-							name='email'
-							id='outlined-basic'
-							label='Email'
-							variant='outlined'
-							color='secondary'
-							required
-						/>
-						<TextField
-							className='textfield'
-							type='text'
-							name='subject'
-							id='outlined-basic'
-							label='Subject'
-							variant='outlined'
-							color='secondary'
-						/>
-						<TextField
-							className='textfield'
-							name='message'
-							id='outlined-static'
-							label='Message'
-							multiline
-							rows='4'
-							variant='outlined'
-							color='secondary'
-							required
-						/>
-						<Button className={classes.button} type='submit' variant='contained' color='primary'>
-							Send
-						</Button>
-					</FormGroup>
+						<div class='field'>
+							<label>First Name</label>
+							<input type='text' name='first-name' placeholder='First Name' />
+						</div>
+						<div class='field'>
+							<label>Last Name</label>
+							<input type='text' name='last-name' placeholder='Last Name' />
+						</div>
+						<button class='ui button' type='submit'>
+							Submit
+						</button>
+					</form>
 				</Grid>
 			</Grid>
 		</div>
